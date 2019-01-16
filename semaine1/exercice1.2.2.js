8c7f18da665a7c4fd24202ad586752e7100604fb
advancod <advancod92@gmail.com>
2019-01-16 22:43:30 +0100

    Correction

--- semaine1/exercice1.2.2.js
+++ semaine1/exercice1.2.2.js
@@ -3,7 +3,7 @@
 // On tri les transactions par efficacité dans un nouveau tableau a deux dimensions
 // On injecte une a une les transactions du nouveau tableau dans une pile de transaction si le total de pourboire ne dépasse pas 6000
 
-let transactions = [
+transactions = [
     [2000, 13000],
     [6000, 9000],
     [800, 2000],
@@ -14,44 +14,72 @@ let transactions = [
     [600, 1500]
    ];
 
-let pile = [];
-let ratios = [];
-let ratiosSorted = [];
-let transactionsSorted = [];
+transactionsSorted = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
+   ];
 
-let increment = 0;
-let total = 0;
-let maxBlock = 6000;
-let currentRatio = 0;
+pile = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
+   ];
 
-for (i=0; i<length.transactions; i++){
+ratios = [];
+ratiosSorted = [];
+ratiosTemp= [];
+
+decrement = transactions.length - 1;
+total = 0;
+maxBlock = 6000;
+
+for (i=0; i<transactions.length; i++){
     ratios[i]=transactions[i][1]/transactions[i][0];
+    ratiosTemp[i]=transactions[i][1]/transactions[i][0];
 }
 
-ratiosSorted = ratios.sort();
-for (j=0; j<length.ratiosSorted; j++){
-    currentRatio = ratiosSorted[j]
-    for (k=0; k<length.transactions; k++){
-        if (ratios[k] == currentRatio){
+console.log("ratios : " + ratios);
+ratiosSorted = ratiosTemp.sort();
+console.log("ratiosTriés : " + ratiosSorted);
+
+for (j=0; j<ratiosSorted.length; j++){
+    console.log("ratio à trouver: " + ratiosSorted[j]);
+    for (k=0; k<transactions.length; k++){
+    console.log("transaction : " + transactions[k]);
+    console.log("ratios : " + ratios[k]);
+        if (ratios[k] == ratiosSorted[j]){
             transactionsSorted[j][0] = transactions[k][0];
             transactionsSorted[j][1] = transactions[k][1];
+            transactions[k] = [0, 0];
         }
     }
 }
 
-while(total < maxBlock){
-    pile[increment][0] = transactionsSorted[increment][0];
-    pile[increment][1] = transactionsSorted[increment][1];
-    total = total +  pile[increment][1];
+console.log("transactions triées : " + ratiosSorted[j]);
+console.log(transactionsSorted);
+
+while(total < maxBlock && decrement > 0 ){
+    pile[decrement][0] = transactionsSorted[decrement][0];
+    pile[decrement][1] = transactionsSorted[decrement][1];
+    console.log(transactionsSorted[decrement]);
+    total = total + pile[decrement][0];
     if (total > maxBlock){
-        pile[increment][0] = 0;
-        pile[increment][1] = 0;
+        pile[decrement][0] = 0;
+        pile[decrement][1] = 0;
+        total = total - pile[decrement][0];
     }
-    increment = increment + 1;
+    decrement = decrement - 1;
 }
-
+console.log("voici la liste des transaction à intégrer de le block de 6000 pour se faire plaiz : ");
 console.log(pile);
-
-
-
-
