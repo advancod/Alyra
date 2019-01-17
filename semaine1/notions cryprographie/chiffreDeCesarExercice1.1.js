412680179222c3185c841479fd600354eea3a4e0
advancod <advancod92@gmail.com>
2019-01-17 10:41:41 +0100

    Le chiffre de César exercice 1.1

---
+++ semaine1/notions cryprographie/chiffreDeCesarExercice1.1.js
@@ -0,0 +1,10 @@
+texteCesar = 'abc';
+cleCesar = 1;
+
+arrayTexteCesar = Array.from(texteCesar);
+arrayTexteChiffreCesar = [];
+
+for (i=0; i<arrayTexteCesar.length; i++){
+arrayTexteChiffreCesar.push(String.fromCodePoint(arrayTexteCesar[i].charCodeAt(0)+cleCesar));
+}
+console.log(arrayTexteChiffreCesar.join(''));
