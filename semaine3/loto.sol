2a6b4aa3ec017318121162c78ce1507d2ea4666a
advancod <advancod92@gmail.com>
2019-01-31 17:31:16 +0100

    commit du bon fichier

--- semaine3/loto.sol
+++ semaine3/loto.sol
@@ -5,24 +5,22 @@ contract Loto {
 
    struct ticketStruct {
     uint8 NumberChoiced;
-    uint256 TimeCreation;
     address playerAddress;
+    uint256 date;
   }
 
   uint256 ticketPrice;
-  uint256 endOfAGame;
   uint256 day1stGameStarted;
   uint256 timeToLiveGame;
   ticketStruct[] tickets;
   address owner;
   address[] win;
   uint256 totalCagnote;
+  uint256 gains;
 
   constructor() public {
     owner = msg.sender;
-    ticketPrice = 100 finney;
-    timeToLiveGame = 5 days;
-    day1stGameStarted = now;
+    ticketPrice = .001 ether;
     totalCagnote = 0;
   }
 
@@ -36,11 +34,22 @@ contract Loto {
     _;
   }
 
-  //acheter ticket
-  function buyTicket() public notOwner {
+  //acheter ticket aléatoire
+  function buyTicketRandom(uint256 choiceDate) public payable notOwner {
+    require(msg.value == ticketPrice);
     tickets[tickets.length-1].NumberChoiced = chooseRandomNumber();
-    tickets[tickets.length-1].TimeCreation = now;
     tickets[tickets.length-1].playerAddress = msg.sender;
+    tickets[tickets.length-1].date = choiceDate;
+    totalCagnote += ticketPrice;
+  }
+
+   //acheter ticket choisi
+  function buyTicket(uint8 number, uint256 choiceDate) public payable notOwner {
+    require(msg.value == ticketPrice);
+    tickets[tickets.length-1].NumberChoiced = number;
+    tickets[tickets.length-1].playerAddress = msg.sender;
+    tickets[tickets.length-1].date = choiceDate;
+    totalCagnote += ticketPrice;
   }
 
   //choisir nombre aléatoire
@@ -48,30 +57,26 @@ contract Loto {
       return uint8(uint256(keccak256(block.timestamp, block.difficulty))%251);
   }
 
-  //jouer automatiquement tous les jeux
-  function buyRandomTicketPeriodicaly() public {
-
-  }
-
-  //finir jeu tous les 5 jours
-  function closeGame() public {
-
-  }
-
-  //retirer ces gains
-  function sendCash() public {
-
+  //retirer les gains
+  function sendCash() onlyOwner public {
+      resultGame();
+      gains = totalCagnote/win.length;
+      for  (uint i=0; i<win.length; i++){
+        win[i].transfer(gains);
+        totalCagnote -= gains;
+    }
   }
 
   //resultat jeu
-  function resultGame() onlyOwner public {
+  function resultGame() private {
     uint number = chooseRandomNumber();
     for  (uint i=0; i<win.length; i++){
      delete win[i];
     }
     for  (uint j=0; j<tickets.length; j++){
-      if  (tickets[j].NumberChoiced==number){
+      if  (tickets[j].NumberChoiced==number && tickets[j].date < now){
         win[j] = tickets[j].playerAddress ;
+        delete tickets[j];
       }else delete tickets[j];
     }
   }
