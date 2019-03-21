d851628a38cb171df1d6177e77867faff8b7b540
advancod <advancod92@gmail.com>
2019-03-21 19:10:21 +0100

    transfer()

--- project/myProject/contracts/Cagnottes.sol
+++ project/myProject/contracts/Cagnottes.sol
@@ -4,7 +4,7 @@ import "./Gimicoin.sol";
 
 contract Cagnottes is Gimicoin  {
 
-event creationCanal(uint groupe, uint montant, uint delai, string pseudo,string description);
+event creationCanal(uint groupe, uint montant, uint delai, string pseudo, string description);
 
 Gimicoin public myGimicoin;
 
@@ -12,11 +12,12 @@ mapping (uint => channel) public mappChannel;
 mapping (uint => address[]) public mappGroupe;
 mapping (uint => address[]) public mappGroupeAdmin;
 mapping (string => address) private mappPseudo;
+mapping (uint => uint[]) public mappChannels;
 
 enum EtatCanal {ACTIF, FERME}
 
 uint IDGroupe;
-uint MAX_MEMBRE = 20;
+uint MAX_MEMBRE = 40;
 uint MAX_DELAI = 50;
 uint MAX_AMOUNT = 50;
 
@@ -27,6 +28,7 @@ struct channel
   uint blocFermeture;
   EtatCanal etat;
   uint groupe;
+  uint enCours;
 }
 
 constructor() public
@@ -48,11 +50,14 @@ function creerCanal(uint _groupe, uint _montant, uint _delai, string memory _pse
       if (mappGroupe[_groupe][i] == msg.sender)
       {
         uint channelID = uint(keccak256(bytes(_pseudo)));
+        require(mappChannel[channelID].etat!=EtatCanal.ACTIF);
         mappChannel[channelID].montant=_montant;
+        mappChannel[channelID].enCours=0;
         mappChannel[channelID].etat=EtatCanal.ACTIF;
         mappChannel[channelID].blocFermeture=block.number+_delai;
         mappChannel[channelID].groupe = _groupe;
         mappChannel[channelID].demandeur = msg.sender;
+        mappChannels[_groupe].push(channelID);
         emit creationCanal(_groupe,_montant,_delai,_pseudo,_description);
         break;
       }
@@ -61,24 +66,34 @@ function creerCanal(uint _groupe, uint _montant, uint _delai, string memory _pse
 
 function payerCanal(uint _groupe, uint _channelID, string memory _pseudo) payable public
 {
-    require(mappPseudo[_pseudo]==msg.sender);
-    for (uint i=0;i<mappGroupe[_groupe].length;i++){
+  bool goodChannel;
+  for (uint j=0;j<mappChannels[_groupe].length;j++){
+    if (mappChannels[_groupe][j] == _channelID)
+    {
+    goodChannel=true;
+    break;
+    }
+  }
+  require(mappChannel[_channelID].etat==EtatCanal.ACTIF);
+  require(mappChannel[_channelID].blocFermeture>=block.number);
+  require(goodChannel==true);
+  require(mappPseudo[_pseudo]==msg.sender);
+  for (uint i=0;i<mappGroupe[_groupe].length;i++){
       if (mappGroupe[_groupe][i] == msg.sender)
       {
-      require(mappChannel[_channelID].etat==EtatCanal.ACTIF);
-      require(mappChannel[_channelID].groupe==_groupe);
-      require(mappChannel[_channelID].etat==EtatCanal.ACTIF);
-      require(mappChannel[_channelID].blocFermeture>=block.number);
+      mappChannel[_channelID].enCours+=msg.value;
       myGimicoin.mint(msg.sender,msg.value);
       break;
       }
   }
 }
 
-function fermetureCanal(uint _channelID, uint _nonce, uint) public {
+function fermetureCanal(uint _channelID) public {
   require(mappChannel[_channelID].blocFermeture<=block.number);
   require(mappChannel[_channelID].etat==EtatCanal.ACTIF);
-  mappChannel[_tokenID].etat=EtatCanal.FERME;
+  require(mappChannel[_channelID].demandeur==msg.sender);
+  msg.sender.transfer(mappChannel[_channelID].enCours);
+  mappChannel[_channelID].etat=EtatCanal.FERME;
 }
 
 function creerPseudo(string memory _pseudo) public
@@ -101,8 +116,8 @@ require(mappGroupe[_groupe].length<MAX_MEMBRE);
 
 function ajouterAdmin(address _membre, uint _groupe) public
 {
-  for (uint i=0;i<mappGroupeAdmin[_groupe].length;i++){
-      if (mappGroupeAdmin[_groupe][i] == msg.sender)
+  for (uint j=0;j<mappGroupeAdmin[_groupe].length;j++){
+      if (mappGroupeAdmin[_groupe][j] == msg.sender)
       {
         for (uint i=0;i<mappGroupe[_groupe].length;i++){
             if (mappGroupe[_groupe][i] == _membre)
