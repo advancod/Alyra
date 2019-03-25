0e1ab7003c9c2e79845c7359c76b6927c1eb2218
advancod <advancod92@gmail.com>
2019-03-25 02:26:09 +0100

    decoupage

--- project/src/contracts/Cagnottes.sol
+++ project/src/contracts/Cagnottes.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
 
 import "./Gimicoin.sol";
 import "./Ownable.sol";
@@ -8,27 +7,15 @@ contract Cagnottes is Gimicoin, Ownable {
 
 Gimicoin public myGimicoin;
 
-mapping (uint => channel) private mappChannel;
+mapping (uint => channel) internal mappChannel;
+mapping (address => uint[]) internal mappGroupesForAddress;
+mapping (string => uint[]) internal mappGroupeAndChannels;
+mapping (uint => string) internal mappIDGroupe;
+
 mapping (string => uint) private mappNomGroupe;
 mapping (string => address[]) private mappGroupe;
-mapping (string => uint[]) private mappGroupeAndChannels;
 mapping (string => address[]) private mappGroupeAdmin;
 mapping (string => address) private mappPseudo;
-mapping (address => uint[]) private mappGroupesForAddress;
-mapping (uint => string) private mappIDGroupe;
-
-//securite
-mapping (address => bool) private blackList;
-function blackLister(address _individu) public onlyOwner
-{
-  blackList[msg.sender]=true;
-}
-modifier notBlackListed() {
-  require(blackList[msg.sender]==false);
-  _;
-}
-
-enum EtatCanal {ACTIF, FERME}
 
 uint MAX_MEMBRE;
 uint MAX_DELAI;
@@ -36,7 +23,6 @@ uint MAX_AMOUNT;
 uint MIN_AMOUNT;
 uint PRICE_RATIO;
 uint MAX_GROUPS;
-uint GROUP_PRICE;
 
 struct channel
 {
@@ -44,8 +30,7 @@ struct channel
   string pseudo;
   uint montant;
   uint blocFermeture;
-  EtatCanal etat;
-  string groupe;
+  bool actif;
   uint enCours;
   uint donnations;
   string description;
@@ -53,62 +38,19 @@ struct channel
 
 constructor() public
 {
-  GROUP_PRICE=1;
-  MAX_GROUPS=50;
+  MAX_GROUPS = 50;
   MAX_MEMBRE = 10;
-  MAX_DELAI = 10;
+  MAX_DELAI = 20;
   MAX_AMOUNT = 10000000000000;
   MIN_AMOUNT = 100000;
-  PRICE_RATIO = 10;
+  PRICE_RATIO = 100;
 }
 
-function modifierMaxGroups(uint _maxGroups) public onlyOwner
+function creerGroupe(string memory _nom, string memory _pseudo) payable public
 {
-  require(_maxGroups >= 10 && _maxGroups <= 1000);
-  MAX_GROUPS = _maxGroups;
-}
-
-function prixGroup(uint _priceGroup) public onlyOwner
-{
-  GROUP_PRICE = _priceGroup;
-}
-
-function modifierDelai(uint _maxDelai) public onlyOwner
-{
-  require(_maxDelai >= 10 && _maxDelai <= 1000000000);
-  MAX_DELAI = _maxDelai;
-}
-
-function modifierMaxMembre(uint _maxMembre) public onlyOwner
-{
-  require(_maxMembre >= 5 && _maxMembre <= 200);
-  MAX_MEMBRE = _maxMembre;
-}
-
-function modifierMaxAmount(uint _maxAmount) public onlyOwner
-{
-  require(_maxAmount >= 10000000 && _maxAmount <= 10000000000000);
-  MAX_AMOUNT = _maxAmount;
-}
-
-function modifierMinAmount(uint _minAmount) public onlyOwner
-{
-  require(_minAmount >= 100000 && _minAmount <= 1000000);
-  MIN_AMOUNT = _minAmount;
-}
-
-function modifierPriceChannel(uint _priceChannel) public onlyOwner
-{
-  require(_priceChannel > 1);
-  PRICE_RATIO = _priceChannel;
-}
-
-function creerGroupe(string memory _nom, string memory _pseudo) payable notBlackListed public
-{
-  require(msg.value>=GROUP_PRICE);
-  require(mappNomGroupe[_nom]==0);
-  require(mappGroupesForAddress[msg.sender].length<MAX_GROUPS);
-  require(mappPseudo[_pseudo]==address(0));
+  require(mappNomGroupe[_nom] == 0);
+  require(mappGroupesForAddress[msg.sender].length < MAX_GROUPS);
+  require(mappPseudo[_pseudo] == address(0));
   uint IDGroupe = uint(keccak256(bytes(_nom)));
   mappNomGroupe[_nom] = IDGroupe;
   mappIDGroupe[IDGroupe] = _nom;
@@ -120,9 +62,9 @@ function creerGroupe(string memory _nom, string memory _pseudo) payable notBlack
 
 function ajouterMembre(address _membre, string memory _groupe, string memory _pseudo) public
 {
-  require(mappGroupe[_groupe].length<MAX_MEMBRE);
-  require(mappPseudo[_pseudo]==address(0));
-  for (uint i=0;i<mappGroupeAdmin[_groupe].length;i++)
+  require(mappGroupe[_groupe].length < MAX_MEMBRE);
+  require(mappPseudo[_pseudo] == address(0));
+  for (uint i = 0;i < mappGroupeAdmin[_groupe].length;i++)
   {
       if (mappGroupeAdmin[_groupe][i] == msg.sender)
       {
@@ -137,11 +79,11 @@ function ajouterMembre(address _membre, string memory _groupe, string memory _ps
 
 function ajouterAdmin(address _membre, string memory _groupe) public
 {
-  for (uint j=0;j<mappGroupeAdmin[_groupe].length;j++)
+  for (uint j = 0;j < mappGroupeAdmin[_groupe].length;j++)
   {
       if (mappGroupeAdmin[_groupe][j] == msg.sender)
       {
-        for (uint i=0;i<mappGroupe[_groupe].length;i++)
+        for (uint i = 0;i < mappGroupe[_groupe].length;i++)
         {
             if (mappGroupe[_groupe][i] == _membre)
             {
@@ -157,37 +99,36 @@ function ajouterAdmin(address _membre, string memory _groupe) public
 function creerCanal(string memory _pseudo, string memory _groupe) private
 {
     uint channelID = uint(keccak256(bytes(_pseudo)));
-    require(mappPseudo[_pseudo]==msg.sender);
-    mappChannel[channelID].etat=EtatCanal.FERME;
-    mappChannel[channelID].pseudo=_pseudo;
-    mappChannel[channelID].groupe = _groupe;
+    require(mappPseudo[_pseudo] == msg.sender);
+    mappChannel[channelID].actif = false;
+    mappChannel[channelID].pseudo = _pseudo;
     mappChannel[channelID].demandeur = msg.sender;
     mappGroupeAndChannels[_groupe].push(channelID);
 }
 
-function demander(uint _montant, uint _delai, string memory _pseudo, string memory _description) payable notBlackListed public
+function demander(uint _montant, uint _delai, string memory _pseudo, string memory _description) payable public
 {
-    require(_montant > MIN_AMOUNT && _montant < MAX_AMOUNT);
-    require(mappPseudo[_pseudo]==msg.sender);
+    require(_montant >= MIN_AMOUNT && _montant <= MAX_AMOUNT);
+    require(mappPseudo[_pseudo] == msg.sender);
     uint min = uint(_montant / PRICE_RATIO);
     require(msg.value >= min);
     uint channelID = uint(keccak256(bytes(_pseudo)));
-    require(mappChannel[channelID].etat==EtatCanal.FERME);
-    mappChannel[channelID].montant=_montant;
-    mappChannel[channelID].enCours=msg.value - min;
-    mappChannel[channelID].etat=EtatCanal.ACTIF;
-    mappChannel[channelID].blocFermeture=block.number+_delai;
+    require(mappChannel[channelID].actif == false);
+    mappChannel[channelID].montant = _montant;
+    mappChannel[channelID].enCours = msg.value - min;
+    mappChannel[channelID].actif = true;
+    mappChannel[channelID].blocFermeture = block.number+_delai;
     mappChannel[channelID].description = _description;
 }
 
-function payerCanal(string memory _groupe, string memory _pseudo, uint _token) payable notBlackListed public
+function payerCanal(string memory _groupe, string memory _pseudo, uint _token) payable public
 {
   uint _channelID = uint(keccak256(bytes(_pseudo)));
-  require(mappChannel[_channelID].etat==EtatCanal.ACTIF);
-  require(mappChannel[_channelID].blocFermeture>=block.number);
-  require(mappChannel[_channelID].demandeur==msg.sender);
+  require(mappChannel[_channelID].actif == true);
+  require(mappChannel[_channelID].blocFermeture >= block.number);
+  require(mappChannel[_channelID].demandeur == msg.sender);
   require(balanceOf(msg.sender) >= _token);
-  for (uint i=0;i<mappGroupe[_groupe].length;i++)
+  for (uint i = 0;i < mappGroupe[_groupe].length;i++)
   {
       if (mappGroupe[_groupe][i] == msg.sender)
       {
@@ -206,16 +147,18 @@ function payerCanal(string memory _groupe, string memory _pseudo, uint _token) p
   }
 }
 
-function fermetureCanal(string memory _pseudo) notBlackListed public
+function fermetureCanal(string memory _pseudo) public
 {
   uint _channelID = uint(keccak256(bytes(_pseudo)));
-  require(mappChannel[_channelID].blocFermeture<=block.number);
-  require(mappChannel[_channelID].etat==EtatCanal.ACTIF);
-  require(mappChannel[_channelID].demandeur==msg.sender);
+  require(mappChannel[_channelID].blocFermeture <= block.number);
+  require(mappChannel[_channelID].actif == true);
+  require(mappChannel[_channelID].demandeur == msg.sender);
   msg.sender.transfer(mappChannel[_channelID].enCours);
   mappChannel[_channelID].enCours = 0;
-  mappChannel[_channelID].etat=EtatCanal.FERME;
-  mappChannel[_channelID].description="";
+  mappChannel[_channelID].montant = 0;
+  mappChannel[_channelID].blocFermeture = 0;
+  mappChannel[_channelID].actif = false;
+  mappChannel[_channelID].description = "";
 }
 
 function consommerTokens(uint _token) public
@@ -223,26 +166,6 @@ function consommerTokens(uint _token) public
   require(balanceOf(msg.sender) >= _token);
   msg.sender.transfer(_token);
   _burnFrom(msg.sender,_token);
-}
-
-function getGroupesPerAddress() public view returns (uint[] memory)
-{
-  return mappGroupesForAddress[msg.sender];
-}
-
-function getMembres(string memory _groupe) public view returns (uint[] memory)
-{
-return mappGroupeAndChannels[_groupe];
-}
-
-function getNomMembre(uint _ID) public view returns (string memory)
-{
-return mappChannel[_ID].pseudo;
-}
-
-function getNomGroupe(uint _ID) public view returns (string memory)
-{
-return mappIDGroupe[_ID];
 }
 
 }
