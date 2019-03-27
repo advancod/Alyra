39da201eea254f81254632f968feecf17f5ffe21
advancod <advancod92@gmail.com>
2019-03-28 00:39:23 +0100

    On est sage et on attend demain

--- project/Ether-Union/contracts/Cagnottes.sol
+++ project/Ether-Union/contracts/Cagnottes.sol
@@ -23,9 +23,10 @@ uint public PRICE_RATIO;
 uint public TIME_TOKEN_WITHDRAW;
 uint public LAPS_TIME_TOKEN_WITHDRAW;
 uint public TOKEN_BONUS;
-uint public DESERVERVE_BONUS;
+uint public DESERVE_BONUS;
 uint public PRICE_GROUP;
 uint public PRICE_MEMBRE;
+uint public PRICE_CHANEL;
 
 struct channel
 {
@@ -33,24 +34,25 @@ struct channel
   string pseudo;
   string groupe;
   uint montant;
-  bool actif;
   uint enCours;
   uint donnations;
   uint receptions;
   address contratCible;
+  string description;
 }
 
 constructor() public
 {
-  MAX_AMOUNT = 10000000000000;
+  MAX_AMOUNT = 100000000000000;
   MIN_AMOUNT = 10000;
   PRICE_RATIO = 100;
   TIME_TOKEN_WITHDRAW = 10000000000;
   LAPS_TIME_TOKEN_WITHDRAW = 100000;
   TOKEN_BONUS = 0;
-  DESERVERVE_BONUS = 1000000;
+  DESERVE_BONUS = 1000000;
   PRICE_GROUP = 1000;
   PRICE_MEMBRE = 100;
+  PRICE_CHANEL = 10;
 }
 
 function creerGroupe(string memory _nom, string memory _pseudo) payable public
@@ -86,7 +88,6 @@ function creerCanal(string memory _pseudo, string memory _groupe, address _membr
   uint channelID = uint(keccak256(bytes(_pseudo)));
   uint groupeID = uint(keccak256(bytes(_groupe)));
   require(mappPseudo[_pseudo] == _membre);
-  mappChannel[channelID].actif = false;
   mappChannel[channelID].pseudo = _pseudo;
   mappChannel[channelID].groupe = _groupe;
   mappChannel[channelID].demandeur = msg.sender;
@@ -95,14 +96,15 @@ function creerCanal(string memory _pseudo, string memory _groupe, address _membr
   mappPseudoInGroup[groupeID][msg.sender] = channelID;
 }
 
-function demander(uint _montant, string memory _pseudo, address _contratCible) public
+function demander(uint _montant, string memory _pseudo, address _contratCible, string memory _description) payable public
 {
   require(mappPseudo[_pseudo] == msg.sender);
   uint channelID = uint(keccak256(bytes(_pseudo)));
-  require(mappChannel[channelID].actif == false);
+  require(msg.value == PRICE_CHANEL);
+  require(mappChannel[channelID].montant == 0);
   mappChannel[channelID].montant = _montant;
-  mappChannel[channelID].actif = true;
   mappChannel[channelID].contratCible = _contratCible;
+  mappChannel[channelID].description = _description;
 }
 
 function payerCanal(string memory _pseudo, address _address) public payable
@@ -110,7 +112,6 @@ function payerCanal(string memory _pseudo, address _address) public payable
   uint _channelID = uint(keccak256(bytes(_pseudo)));
   require(mappChannel[_channelID].demandeur == _address);
   require(mappChannel[_channelID].montant > mappChannel[_channelID].enCours);
-  require(mappChannel[_channelID].actif == true);
   uint fees = uint(msg.value / PRICE_RATIO);
   _mint(msg.sender,msg.value + fees);
   uint addValue = msg.value;
@@ -123,13 +124,13 @@ function payerCanal(string memory _pseudo, address _address) public payable
 function fermetureCanal(string memory _pseudo) public
 {
   uint _channelID = uint(keccak256(bytes(_pseudo)));
-  require(mappChannel[_channelID].actif == true);
+  require(mappChannel[_channelID].enCours != 0);
   require(mappChannel[_channelID].demandeur == msg.sender);
   msg.sender.transfer(mappChannel[_channelID].enCours);
   mappChannel[_channelID].enCours = 0;
   mappChannel[_channelID].montant = 0;
-  mappChannel[_channelID].actif = false;
   mappChannel[_channelID].contratCible = address(0);
+  mappChannel[_channelID].description = "";
 }
 
 function withdrawEther(uint _amount) public onlyOwner
@@ -140,15 +141,20 @@ function withdrawEther(uint _amount) public onlyOwner
 function tokenToEther() public
 {
   require(block.number % TIME_TOKEN_WITHDRAW  >= 0 && block.number % TIME_TOKEN_WITHDRAW <= LAPS_TIME_TOKEN_WITHDRAW);
-  uint monBonus = uint(balanceOf(msg.sender).div(DESERVERVE_BONUS));
+  uint monBonus = uint(balanceOf(msg.sender).div(DESERVE_BONUS));
   monBonus.mul(TOKEN_BONUS);
   msg.sender.transfer(balanceOf(msg.sender).add(monBonus));
   _burnFrom(msg.sender,balanceOf(msg.sender));
 }
 
+function modifierPriceChannel(uint _priceChannel) public onlyOwner
+{
+  PRICE_CHANEL = _priceChannel;
+}
+
 function modifierMeriteBonus(uint _seuil) public onlyOwner
 {
-  DESERVERVE_BONUS = _seuil;
+  DESERVE_BONUS = _seuil;
 }
 
 function modifierTokenBonus(uint _tokenBonus) public onlyOwner
@@ -199,6 +205,11 @@ function getGroupesPerAddress() public view returns (uint[] memory)
 function getMembres(string memory _groupe) public view returns (uint[] memory)
 {
   return mappGroupeAndChannels[_groupe];
+}
+
+function getDescription(string memory _pseudo) public view returns (string memory)
+{
+  return mappChannel[mappPseudoToID[_pseudo]].description;
 }
 
 function getNomMembre(uint _ID) public view returns (string memory)
